2bbd16cf57005c0579a77f0bd286d1669bef68b2
Massacrer <massacrer@hotmail.co.uk>
2012-10-22 22:42:22 +0100

    Tidied up solver framework, defined most(?) necessary methods for init

--- src/me/will_s/school/sudoku/Solver.java
+++ src/me/will_s/school/sudoku/Solver.java
@@ -15,30 +15,59 @@ public class Solver {
 	// by UI-level code.
 	// Settling on List<List<Integer>> for now.
 	public static void solve(List<List<Integer>> grid) {
-		Initialiser.initHeaders();
-		Initialiser.setupNodesFromInput(grid);
+		Initialiser.initialise(grid);
 		// DLX goes here
 	}
 }
 
 class Initialiser {
+	
+	public static void initialise(List<List<Integer>> grid) {
+		initHeaders();
+		initConstraints();
+		Initialiser.coverNodesFromInput(grid);
+	}
+	
 	// Init column headers
 	// Columns represent solution parts in the Sudoku grid e.g. (R1C2 = 3)
 	// There are a total of 9^3 = 729 headers to create
 	public static void initHeaders() {
 		Node root = RootNode.get();
 		Node left = root;
+		Node right;
 		for (int r = 1; r <= 9; r++) {
 			for (int c = 1; c <= 9; c++) {
 				for (int v = 1; v <= 9; v++) {
-					// Init header nodes with identifiers and link them into the
-					// grid here
+					HeaderNode n = new HeaderNode(left, root, new SolutionPart(
+							r, c, v));
+					// TODO: check if final assignment to n.right.left works as
+					// expected
+					n.left.right = n;
+					if (r == 9 && c == 9 && v == 9) {
+						n.right.left = n;
+					}
+					// Advance (pointer to prev. element) to current element and
+					// end loop
+					left = n;
 				}
 			}
 		}
 	}
 	
-	public static void setupNodesFromInput(List<List<Integer>> grid) {
+	// Initialise all constraints - link each RCV with the constraints it
+	// fulfils by creation of a node at the point of intersection
+	public static void initConstraints() {
+		
+	}
+	
+	// Called from initConstraints(), sets up a single mapping of an RCV (header
+	// node) to the constraints it covers
+	static void link(int row, int column, int value) {
+		
+	}
+	
+	// Given the full 729x324 sparse grid, remove given numbers as possibilities
+	public static void coverNodesFromInput(List<List<Integer>> grid) {
 		// Have fun with this :)
 	}
 }
@@ -51,45 +80,63 @@ class Node {
 	public Node right;
 	
 	// Constructor time
-	// TODO: only implement one constructor, figure out which of these would be
-	// more efficient
+	// TODO: implement as few constructors as possible
+	
+	protected Node() {
+		this.head = this;
+		this.up = this;
+		this.down = this;
+		this.left = this;
+		this.right = this;
+		Solver.nodes.add(this);
+	}
+	
 	public Node(Node head) {
+		this();
 		this.head = head;
-		this.point((byte) 0x15, null);
-		Solver.nodes.add(this);
 	}
 	
-	public Node(Node head, Node up, Node down, Node left, Node right) {
-		this.head = head;
-		this.up = up;
-		this.down = down;
-		this.left = left;
-		this.right = right;
-		Solver.nodes.add(this);
-	}
-	
-	public void point(byte mask, Node target) {
-		if (target == null) {
-			target = this;
+	// TODO: Possibly unnecessary, review after init code complete
+	public Node(NodeSet nodes) {
+		for (Node n : nodes.nodes) {
+			if (n == null) {
+				n = this;
+			}
 		}
-		// TODO: test bitmasking and comparison
-		if ((mask & 0x1) == 1)
-			this.up = target;
-		if ((mask & 0x2) == 1)
-			this.down = target;
-		if ((mask & 0x4) == 1)
-			this.left = target;
-		if ((mask & 0x8) == 1)
-			this.right = target;
+		
+		this.head = nodes.nodes[0];
+		this.up = nodes.nodes[1];
+		this.down = nodes.nodes[2];
+		this.left = nodes.nodes[3];
+		this.right = nodes.nodes[4];
+		Solver.nodes.add(this);
+	}
+	
+	class NodeSet {
+		public Node[] nodes = new Node[5];
+		
+		public NodeSet(Node head, Node up, Node down, Node left, Node right) {
+			nodes[0] = head;
+			nodes[1] = up;
+			nodes[2] = down;
+			nodes[3] = left;
+			nodes[4] = right;
+		}
 	}
 }
 
 class HeaderNode extends Node {
-	public SolutionPart solutionPartIdentifier;
+	public SolutionPart solutionPartId;
 	
-	public HeaderNode(Node head, SolutionPart part) {
-		super(head);
-		this.solutionPartIdentifier = part;
+	protected HeaderNode() {
+		super();
+	}
+	
+	public HeaderNode(Node left, Node right, SolutionPart part) {
+		super();
+		this.left = left;
+		this.right = right;
+		this.solutionPartId = part;
 	}
 }
 
@@ -106,8 +153,7 @@ class RootNode extends HeaderNode {
 	private RootNode() {
 		// this.head should never be accessed, so setting it to null will help
 		// uncover some types of bugs in the node walker code
-		super(null, null);
-		this.head = this;
+		super();
 		RootNode.root = this;
 	}
 	
@@ -124,9 +170,10 @@ class SolutionPart {
 	// Assigning 4 bytes to each int, so max unsigned value of each variable =
 	// 2^4 = 16
 	// Order of storage (msb -> lsb) is row, column, value
+	// So data bytes in short looks like 0000rrrrccccvvvv
 	private short info = 0;
 	
-	public SolutionPart(short r, short c, short v) {
+	public SolutionPart(int r, int c, int v) {
 		// Make sure all 3 values are sane for the grid size, and can be stored
 		// in 4 bytes.
 		if ((r < 1) || (r > 9) || (c < 1) || (c > 9) || (v < 1) || (v > 9)) {
@@ -134,27 +181,31 @@ class SolutionPart {
 					+ ",c" + c + ",v" + v);
 		}
 		
-		int i = (r << 8) | (c << 4) | (v);
-		info = (short) i;
+		// TODO: probably not necessary, implement check and confirm at some
+		// point
+		r = r & 0x0000000f;
+		c = c & 0x0000000f;
+		v = v & 0x0000000f;
+		
+		short i = (short) ((r << 8) | (c << 4) | (v));
+		info = i;
 	}
 	
 	public int getRow() {
-		// this step probably not necessary (can probably just use int i = info)
-		// TODO: find out :)
-		Integer i = Integer.valueOf(info);
-		i = i & 0x00ff0000;
+		int i = info;
+		i = i & 0x00000f00;
 		return i >> 8;
 	}
 	
 	public int getColumn() {
-		Integer i = Integer.valueOf(info);
-		i = i & 0x0000ff00;
+		int i = info;
+		i = i & 0x000000f0;
 		return i >> 4;
 	}
 	
 	public int getValue() {
-		Integer i = Integer.valueOf(info);
-		i = i & 0x000000ff;
+		int i = info;
+		i = i & 0x0000000f;
 		return i;
 	}
 }
